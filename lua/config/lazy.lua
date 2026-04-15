283c4ca62925f8a2e9bd2de688052f5da31c25d2
ruzul2185 <ruzulchaure2185@gmail.com>
2026-04-16 00:13:08 +0530

    language server protocal automated

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" },
+    { import = "plugins.lsp" },
   },
   -- automatically check for plugin updates
   checker = { enabled = true },
