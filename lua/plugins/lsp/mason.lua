362fc8ef9073e88039c469b0cbf8f2496f8cdab2
ruzul2185 <ruzulchaure2185@gmail.com>
2026-04-16 16:23:23 +0530

    noice dialog box added

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,6 +1,8 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
+    opts = {
+        ensure_installed = {},
+    },
     dependencies = {
         {
             "mason-org/mason.nvim",
